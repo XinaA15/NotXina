5d23e9d60967294ac05003214d8bff8109e495f9
Roeegh <admin@roeegh.dev>
2023-01-12 16:02:29 -0600

    🌸 Added user count

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'guildMemberAdd',
+	async execute(member) {
+		let channel = await member.guild.channels.fetch('1063213985815597126');
+		channel.setName(`Humans: ${member.guild.memberCount - member.guild.members.cache.filter(m => m.user.bot).size}`).catch(e => console.error(`[ERROR]: ${e}`));
+	}
+};
--- events/onReady.js
+++ events/onReady.js
@@ -7,5 +7,14 @@ module.exports = {
 		console.log(`[CLIENT] Logged in as ${client.user.tag}!`);
 
 		client.user.setPresence({ status: 'dnd', activities: [{ name: 'xina make xinaA15', type: ActivityType.Watching }] });
+
+		client.guilds.fetch(process.env.GUILD).then(guild => {
+			guild.channels
+				.fetch('1063213985815597126')
+				.then(channel => {
+					channel.setName(`Humans: ${guild.memberCount - guild.members.cache.filter(m => m.user.bot).size}`).catch(e => console.error(`[ERROR]: ${e}`));
+				})
+				.catch(e => console.error(`[ERROR]: ${e}`));
+		});
 	}
 };
