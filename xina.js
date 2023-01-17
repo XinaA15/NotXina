5cd7a9339f9011ff31b7a816e52fe17a6cec5aa9
Roeegh <admin@roeegh.dev>
2023-01-16 19:55:12 -0600

    🌸 Remove guild commands before registering global commands

--- xina.js
+++ xina.js
@@ -114,8 +114,8 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
 (async () => {
 	try {
+		await rest.put(Routes.applicationCommands(process.env.CLIENT, process.env.GUILD), { body: [] });
 		// await rest.put(Routes.applicationGuildCommands(process.env.CLIENT, process.env.GUILD), { body: [...Array.from(client.slashCommands.values()).map(c => c.data.toJSON())] });
-		// await rest.put(Routes.applicationCommands(process.env.CLIENT, process.env.GUILD), { body: [] });
 
 		await rest.put(Routes.applicationCommands(process.env.CLIENT), { body: [] });
 		await rest.put(Routes.applicationCommands(process.env.CLIENT), { body: [...Array.from(client.slashCommands.values()).map(c => c.data.toJSON())] });
