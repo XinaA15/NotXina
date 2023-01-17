26d925b722564fc7fd469e4763416e4874022ec0
Roeegh <admin@roeegh.dev>
2023-01-16 19:47:18 -0600

    🌸 Added welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,14 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
 	name: 'guildMemberAdd',
 	async execute(member) {
-		let channel = await member.guild.channels.fetch('1063213985815597126');
-		channel.setName(`Humans: ${member.guild.memberCount - member.guild.members.cache.filter(m => m.user.bot).size}`).catch(e => console.error(`[ERROR]: ${e}`));
+		await member.guild.channels.fetch('1063213985815597126').then(channel => {
+			channel.setName(`Humans: ${member.guild.memberCount - member.guild.members.cache.filter(m => m.user.bot).size}`).catch(e => console.error(`[ERROR]: ${e}`));
+		});
+
+		await member.guild.channels.fetch('1064284756403032115').then(channel => {
+			channel.send(new EmbedBuilder().setTitle(`Welcome ${member.user.tag.toString()}`).setDescription(`Welcome to the server, please read the rules at <#1052664412655329340> and grab roles at <#1061588505928663070>!`).setThumbnail(member.user.displayAvatarURL()).setColor('Random').setTimestamp().setFooter(`ID: ${member.user.id}`)).catch(e => console.error(`[ERROR]: ${e}`));
+		});
 	}
 };
