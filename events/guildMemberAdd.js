0a8323f9409147e63f252038a87a0dab9bcb2d2b
Roeegh <admin@roeegh.dev>
2023-01-16 20:00:04 -0600

    🌸 Attempts to fix join message using shittier code

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,22 +3,20 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
 	name: 'guildMemberAdd',
 	async execute(member) {
-		await member.guild.channels.fetch('1063213985815597126').then(channel => {
-			channel.setName(`Humans: ${member.guild.memberCount - member.guild.members.cache.filter(m => m.user.bot).size}`).catch(e => console.error(`[ERROR]: ${e}`));
-		});
+		let memberCountChannel = await member.guild.channels.fetch('1063213985815597126');
+		memberCountChannel.setName(`Humans: ${member.guild.memberCount - member.guild.members.cache.filter(m => m.user.bot).size}`).catch(e => console.error(`[ERROR]: ${e}`));
 
-		await member.guild.channels.fetch('1064284756403032115').then(channel => {
-			channel
-				.send(
-					new EmbedBuilder()
-						.setTitle(`Welcome ${member.user.tag.toString()}`)
-						.setDescription(`Welcome to the server, please read the rules at <#1052664412655329340> and grab roles at <#1061588505928663070>!`)
-						.setThumbnail(member.user.displayAvatarURL())
-						.setColor('Random')
-						.setTimestamp()
-						.setFooter({ text: `ID: ${member.user.id}` })
-				)
-				.catch(e => console.error(`[ERROR]: ${e}`));
-		});
+		let joinMessageChannel = await member.guild.channels.fetch('1064284756403032115');
+		joinMessageChannel
+			.send(
+				new EmbedBuilder()
+					.setTitle(`Welcome ${member.user.tag.toString()}`)
+					.setDescription(`Welcome to the server, please read the rules at <#1052664412655329340> and grab roles at <#1061588505928663070>!`)
+					.setThumbnail(member.user.displayAvatarURL())
+					.setColor('Random')
+					.setTimestamp()
+					.setFooter({ text: `ID: ${member.user.id}` })
+			)
+			.catch(e => console.error(`[ERROR]: ${e}`));
 	}
 };
